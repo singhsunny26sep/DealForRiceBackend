f85d824b19cd37cfaf624fd83b322b47425a197c
Devashish Rathod <rathod.devashish09@gmail.com>
2026-01-31 00:30:07 +0530

    product delete done

--- controller/product.js
+++ controller/product.js
@@ -1,119 +1,163 @@
 const Product = require("../model/Product");
 const User = require("../model/User");
 const { sendMultipleNotification } = require("../service/notification");
-const { uploadToCloudinary, deleteFromCloudinary } = require("../service/uploadImage")
-const mongoose = require('mongoose');
-
+const {
+  uploadToCloudinary,
+  deleteFromCloudinary,
+} = require("../service/uploadImage");
+const mongoose = require("mongoose");
 
 exports.getAllProducts = async (req, res) => {
-    const id = req.params?.id
-    try {
-        if (id) {
-            const product = await Product.findById(id).populate({ path: "user", select: "-password -createdAt -updatedAt -__v -role" }).populate("trade")
-            if (product) {
-                return res.status(200).json({ success: true, msg: "Product found", result: product });
-            }
-            return res.status(404).json({ success: false, msg: "Product not found" });
-        }
-        const result = await Product.find().sort({ createdAt: -1 }).populate({ path: "user", select: "-password -createdAt -updatedAt -__v -role" }).populate("trade")
-        if (!result) {
-            return res.status(404).json({ success: false, msg: "No products found" });
-        }
-        return res.status(200).json({ success: true, msg: "All products found", result });
-    } catch (error) {
-        console.log("error on getAllProducts: ", error);
-        return res.status(500).json({ error: error, success: false, msg: error.message })
+  const id = req.params?.id;
+  try {
+    if (id) {
+      const product = await Product.findById(id)
+        .populate({
+          path: "user",
+          select: "-password -createdAt -updatedAt -__v -role",
+        })
+        .populate("trade");
+      if (product) {
+        return res
+          .status(200)
+          .json({ success: true, msg: "Product found", result: product });
+      }
+      return res.status(404).json({ success: false, msg: "Product not found" });
     }
-}
-
+    const result = await Product.find()
+      .sort({ createdAt: -1 })
+      .populate({
+        path: "user",
+        select: "-password -createdAt -updatedAt -__v -role",
+      })
+      .populate("trade");
+    if (!result) {
+      return res.status(404).json({ success: false, msg: "No products found" });
+    }
+    return res
+      .status(200)
+      .json({ success: true, msg: "All products found", result });
+  } catch (error) {
+    console.log("error on getAllProducts: ", error);
+    return res
+      .status(500)
+      .json({ error: error, success: false, msg: error.message });
+  }
+};
 
 exports.getLatestProducts = async (req, res) => {
-    try {
-        const result = await Product.find().sort({ createdAt: -1 }).limit(5).populate({ path: "user", select: "-password -createdAt -updatedAt -__v -role" }).populate("trade")
-        if (!result) {
-            return res.status(404).json({ success: false, msg: "No products found" });
-        }
-        return res.status(200).json({ success: true, msg: "Latest products found", result });
-    } catch (error) {
-        console.log("error on getLatestProducts: ", error);
-        return res.status(500).json({ error: error, success: false, msg: error.message })
+  try {
+    const result = await Product.find()
+      .sort({ createdAt: -1 })
+      .limit(5)
+      .populate({
+        path: "user",
+        select: "-password -createdAt -updatedAt -__v -role",
+      })
+      .populate("trade");
+    if (!result) {
+      return res.status(404).json({ success: false, msg: "No products found" });
     }
-}
+    return res
+      .status(200)
+      .json({ success: true, msg: "Latest products found", result });
+  } catch (error) {
+    console.log("error on getLatestProducts: ", error);
+    return res
+      .status(500)
+      .json({ error: error, success: false, msg: error.message });
+  }
+};
 
 exports.getProductByUserId = async (req, res) => {
-    const id = req.params?.id // user id
-
-    try {
-        const checkUser = await User.findById(id)
-
-        if (!checkUser) {
-            return res.status(400).json({ success: false, msg: 'User not found!' })
-        }
-        const result = await Product.find({ user: id }).populate({ path: "user", select: "-password" }).populate("trade")
-
-        if (result) {
-            return res.status(200).json({ success: true, msg: "Products found", result });
-        }
-        return res.status(404).json({ success: false, msg: "No products found" });
-    } catch (error) {
-        console.log("error on getProductByUserId: ", error);
-        return res.status(500).json({ error: error, success: false, msg: error.message })
+  const id = req.params?.id; // user id
+  try {
+    const checkUser = await User.findById(id);
+    if (!checkUser) {
+      return res.status(400).json({ success: false, msg: "User not found!" });
     }
-}
+    const result = await Product.find({ user: id })
+      .populate({ path: "user", select: "-password" })
+      .populate("trade");
+    if (result) {
+      return res
+        .status(200)
+        .json({ success: true, msg: "Products found", result });
+    }
+    return res.status(404).json({ success: false, msg: "No products found" });
+  } catch (error) {
+    console.log("error on getProductByUserId: ", error);
+    return res
+      .status(500)
+      .json({ error: error, success: false, msg: error.message });
+  }
+};
 
 exports.addProduct = async (req, res) => {
-    const name = req.body?.name
-    const description = req.body?.description
-    const price = req.body?.price
-    const image = req.files?.image
-    const id = req.payload?._id //uesr id of created product
-    const trade = req.body?.trade
-
-    // console.log("req.body: ", req.body);
-
-
-    try {
-        if (!name) {
-            return res.status(400).json({ success: false, msg: 'Name is required!' })
-        }
-        if (!price) {
-            return res.status(400).json({ success: false, msg: 'Price is required!' })
-        }
-        const product = new Product({ name, description, price, user: id })
-        if (image) {
-            let imageUrl = await uploadToCloudinary(image.tempFilePath)
-            product.image = imageUrl
-        }
-        if (mongoose.Types.ObjectId.isValid(trade)) product.trade = trade
-
-        const result = await product.save()
-        if (result) {
-            await sendMultipleNotification(name, description, "added", "Product", id)
-            return res.status(200).json({ success: true, msg: 'Product added successfully', result })
-        }
-        return res.status(400).json({ success: false, msg: 'Failed to add product!' })
-    } catch (error) {
-        console.log("error on addProduct: ", error);
-        return res.status(500).json({ error: error, success: false, msg: error.message })
+  const name = req.body?.name;
+  const description = req.body?.description;
+  const price = req.body?.price;
+  const image = req.files?.image;
+  const id = req.payload?._id; //uesr id of created product
+  const trade = req.body?.trade;
+  // console.log("req.body: ", req.body);
+  try {
+    if (!name) {
+      return res.status(400).json({ success: false, msg: "Name is required!" });
     }
-}
+    if (!price) {
+      return res
+        .status(400)
+        .json({ success: false, msg: "Price is required!" });
+    }
+    const product = new Product({ name, description, price, user: id });
+    if (image) {
+      let imageUrl = await uploadToCloudinary(image.tempFilePath);
+      product.image = imageUrl;
+    }
+    if (mongoose.Types.ObjectId.isValid(trade)) product.trade = trade;
+
+    const result = await product.save();
+    if (result) {
+      await sendMultipleNotification(name, description, "added", "Product", id);
+      return res
+        .status(200)
+        .json({ success: true, msg: "Product added successfully", result });
+    }
+    return res
+      .status(400)
+      .json({ success: false, msg: "Failed to add product!" });
+  } catch (error) {
+    console.log("error on addProduct: ", error);
+    return res
+      .status(500)
+      .json({ error: error, success: false, msg: error.message });
+  }
+};
 
 exports.getAdminProduct = async (req, res) => {
-    try {
-        // Fetch products where user is "admin" with pagination
-        const userIds = await User.find({ role: 'admin' })
-        const result = await Product.find({ user: userIds }).populate({ path: 'user', select: 'name email mobile address city state image', }).populate("trade")
-        // const products = await Product.find().populate({ path: 'user', match: { role: 'admin' }, select: 'name email mobile address city state image', })
-        // const result = products.filter(product => product.user !== null); products
-        if (result) {
-            return res.status(200).json({ success: true, result });
-        }
-        return res.status(404).json({ success: false, msg: "No products found" });
-    } catch (error) {
-        console.log("error on getAdminProduct: ", error);
-        return res.status(500).json({ error: error, success: false, msg: error.message })
+  try {
+    // Fetch products where user is "admin" with pagination
+    const userIds = await User.find({ role: "admin" });
+    const result = await Product.find({ user: userIds })
+      .populate({
+        path: "user",
+        select: "name email mobile address city state image",
+      })
+      .populate("trade");
+    // const products = await Product.find().populate({ path: 'user', match: { role: 'admin' }, select: 'name email mobile address city state image', })
+    // const result = products.filter(product => product.user !== null); products
+    if (result) {
+      return res.status(200).json({ success: true, result });
     }
-}
+    return res.status(404).json({ success: false, msg: "No products found" });
+  } catch (error) {
+    console.log("error on getAdminProduct: ", error);
+    return res
+      .status(500)
+      .json({ error: error, success: false, msg: error.message });
+  }
+};
 /* exports.getAdminProduct = async (req, res) => {
     try {
 
@@ -139,76 +183,82 @@ exports.getAdminProduct = async (req, res) => {
 } */
 
 exports.updateProduct = async (req, res) => {
-
-    const id = req.params?.id // product id
-    const user = req.payload?._id // user id
-    const name = req.body?.name
-    const description = req.body?.description
-    const price = req.body?.price
-    const image = req.files?.image
-    const trade = req.body?.trade
-    try {
-        let query = { _id: id }
-
-        const checkUser = await User.findById(user)
-        if (checkUser.role !== 'admin') {
-            query.user = user
-        }
-
-        const checkProduct = await Product.findOne(query)
-        if (!checkProduct) {
-            return res.status(404).json({ success: false, msg: 'Product not found!' })
-        }
-
-        if (name) checkProduct.name = name
-        if (description) checkProduct.description = description
-        if (price) checkProduct.price = price
-        if (mongoose.Types.ObjectId.isValid(trade)) checkProduct.trade = trade
-
-        if (image) {
-            if (checkProduct.image) {
-                await deleteFromCloudinary(checkProduct?.image)
-            }
-            let imageUrl = await uploadToCloudinary(image.tempFilePath)
-            checkProduct.image = imageUrl
-        }
-
-
-        const result = await checkProduct.save()
-        if (result) {
-            return res.status(200).json({ success: true, msg: 'Product updated successfully', result })
-        }
-        return res.status(400).json({ success: false, msg: 'Failed to update product!' })
-    } catch (error) {
-        console.log("error on updateProduct: ", error);
-        return res.status(500).json({ error: error, success: false, msg: error.message })
+  const id = req.params?.id; // product id
+  const user = req.payload?._id; // user id
+  const name = req.body?.name;
+  const description = req.body?.description;
+  const price = req.body?.price;
+  const image = req.files?.image;
+  const trade = req.body?.trade;
+  try {
+    let query = { _id: id };
+    const checkUser = await User.findById(user);
+    if (checkUser.role !== "admin") {
+      query.user = user;
     }
-}
+    const checkProduct = await Product.findOne(query);
+    if (!checkProduct) {
+      return res
+        .status(404)
+        .json({ success: false, msg: "Product not found!" });
+    }
+    if (name) checkProduct.name = name;
+    if (description) checkProduct.description = description;
+    if (price) checkProduct.price = price;
+    if (mongoose.Types.ObjectId.isValid(trade)) checkProduct.trade = trade;
+    if (image) {
+      if (checkProduct.image) {
+        await deleteFromCloudinary(checkProduct?.image);
+      }
+      let imageUrl = await uploadToCloudinary(image.tempFilePath);
+      checkProduct.image = imageUrl;
+    }
+    const result = await checkProduct.save();
+    if (result) {
+      return res
+        .status(200)
+        .json({ success: true, msg: "Product updated successfully", result });
+    }
+    return res
+      .status(400)
+      .json({ success: false, msg: "Failed to update product!" });
+  } catch (error) {
+    console.log("error on updateProduct: ", error);
+    return res
+      .status(500)
+      .json({ error: error, success: false, msg: error.message });
+  }
+};
 
 exports.deleteProduct = async (req, res) => {
-    const id = req.params?.id // product id
-    const user = req.payload?._id // user id
-    try {
-        const checkUser = await User.findById(user)
-        if (!checkUser) {
-            return res.status(404).json({ success: false, msg: 'User not found!' })
-        }
-
-        const checkProdcut = await Product.findById(id)
-        if (!checkProdcut) {
-            return res.status(404).json({ success: false, msg: 'Product not found!' })
-        }
-        if (checkProdcut.image) {
-            await deleteFromCloudinary(checkUser?.image)
-        }
-        const result = await Product.findByIdAndDelete(id)
-        if (result) {
-            return res.status(200).json({ success: true, msg: `Product ${checkProdcut?.name} deleted successfully` })
-        }
-        return res.status(404).json({ success: false, msg: "Product not found" })
-
-    } catch (error) {
-        console.log("error on deleteProduct: ", error);
-        return res.status(500).json({ error: error, success: false, msg: error.message })
+  const id = req.params?.id; // product id
+  const user = req.payload?._id; // user id
+  try {
+    const checkUser = await User.findById(user);
+    if (!checkUser) {
+      return res.status(404).json({ success: false, msg: "User not found!" });
     }
-}
+    const checkProdcut = await Product.findById(id);
+    if (!checkProdcut) {
+      return res
+        .status(404)
+        .json({ success: false, msg: "Product not found!" });
+    }
+    if (checkProdcut.image) {
+      await deleteFromCloudinary(checkProdcut?.image);
+    }
+    const result = await Product.findByIdAndDelete(id);
+    if (result) {
+      return res.status(200).json({
+        success: true,
+        msg: `Product ${checkProdcut?.name} deleted successfully`,
+      });
+    }
+    return res.status(404).json({ success: false, msg: "Product not found" });
+  } catch (error) {
+    console.log("error on deleteProduct: ", error);
+    return res
+      .status(500)
+      .json({ error: error, success: false, msg: error.message });
+  }
+};
--- router/product.js
+++ router/product.js
@@ -1,23 +1,23 @@
-const express = require('express')
-const { getAllProducts, addProduct, updateProduct, deleteProduct, getProductByUserId, getLatestProducts, getAdminProduct } = require('../controller/product')
-const { verifyToken } = require('../middleware/authValidation')
-const proRouter = express.Router()
+const express = require("express");
+const proRouter = express.Router();
+const {
+  getAllProducts,
+  addProduct,
+  updateProduct,
+  deleteProduct,
+  getProductByUserId,
+  getLatestProducts,
+  getAdminProduct,
+} = require("../controller/product");
+const { verifyToken } = require("../middleware/authValidation");
 
-proRouter.get('/latestProduct', getLatestProducts)
+proRouter.get("/latestProduct", getLatestProducts);
+proRouter.get("/", getAllProducts);
+proRouter.get("/adminProducts", getAdminProduct);
+proRouter.get("/:id", getAllProducts);
+proRouter.get("/brand/user/:id", getProductByUserId);
+proRouter.post("/brand/add", verifyToken, addProduct);
+proRouter.put("/brand/update/:id", verifyToken, updateProduct);
+proRouter.delete("/brand/delete/:id", verifyToken, deleteProduct);
 
-proRouter.get('/', getAllProducts)
-
-proRouter.get('/adminProducts', getAdminProduct)
-
-proRouter.get('/:id', getAllProducts)
-
-proRouter.get('/brand/user/:id', getProductByUserId)
-
-proRouter.post('/brand/add', verifyToken, addProduct)
-
-proRouter.put('/brand/update/:id', verifyToken, updateProduct)
-
-proRouter.delete('/brand/delete/:id', verifyToken, deleteProduct)
-
-
-module.exports = proRouter
+module.exports = proRouter;
