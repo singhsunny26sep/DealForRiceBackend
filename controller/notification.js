832261c75cd6665acef6bda11929ccb36bb3c5a0
Devashish Rathod <rathod.devashish09@gmail.com>
2026-02-08 23:12:24 +0530

    notification sent to all users done

--- controller/notification.js
+++ controller/notification.js
@@ -1,32 +1,51 @@
 const Notification = require("../model/Notification");
-const { sendSingleNotification } = require("../service/notification");
-
+const { sendPushNotification } = require("../service/notification");
 
 exports.getNotification = async (req, res) => {
-    const id = req.params?.id
-    try {
-        const result = await Notification.find({ userId: id }).sort({ createdAt: -1 })
-        if (result) {
-            return res.status(200).json({ success: true, result })
-        }
-        return res.status(404).json({ success: false, msg: "No notifications found" });
-    } catch (error) {
-        console.log("error on getNotification: ", error);
-        return res.status(500).json({ error: error, success: false, msg: error.message })
+  const id = req.params?.id;
+  try {
+    const result = await Notification.find({ userId: id }).sort({
+      createdAt: -1,
+    });
+    if (result) {
+      return res.status(200).json({ success: true, result });
     }
-}
-
+    return res
+      .status(404)
+      .json({ success: false, msg: "No notifications found" });
+  } catch (error) {
+    console.log("error on getNotification: ", error);
+    return res
+      .status(500)
+      .json({ error: error, success: false, msg: error.message });
+  }
+};
 
 exports.sendTestNotification = async (req, res) => {
-    const msg = req.body?.msg
-    const fcmToken = req.body?.fcmToken
-    try {
-        await sendSingleNotification(msg, msg, "67b72a2e3b5e80bd0a6c44dc", fcmToken, "notification", "Notification")
-        console.log(" ======================================== notification sent ========================================");
-
-        return res.status(200).json({ success: true, msg: "Notification sent successfully" });
-    } catch (error) {
-        console.log("error on sendTestNotification: ", error);
-        return res.status(500).json({ error: error, success: false, msg: error.message })
-    }
-}
+  const msg = req.body?.msg;
+  const fcmToken = req.body?.fcmToken;
+  try {
+    await sendPushNotification(
+      "67b72a2e3b5e80bd0a6c44dc",
+      {
+        title: "Test Notification",
+        body: msg,
+        senderId: "67b72a2e3b5e80bd0a6c44dc",
+      },
+      fcmToken,
+      "notification",
+      "Notification",
+    );
+    console.log(
+      " ======================================== notification sent ========================================",
+    );
+    return res
+      .status(200)
+      .json({ success: true, msg: "Notification sent successfully" });
+  } catch (error) {
+    console.log("error on sendTestNotification: ", error);
+    return res
+      .status(500)
+      .json({ error: error, success: false, msg: error.message });
+  }
+};
--- service/notification.js
+++ service/notification.js
@@ -1,116 +1,58 @@
-// const admin = require("firebase-admin");
-// const serviceAccount = require("../firebaseSecretKeys.json");
-// const Notification = require("../model/Notification");
-// const User = require("../model/User");
-// admin.initializeApp({
-//   credential: admin.credential.cert(serviceAccount),
-// });
-
-// exports.sendSingleNotification = async (
-//   title,
-//   description,
-//   userId,
-//   fcmToken,
-//   action,
-//   targetType,
-//   image
-// ) => {
-//   const messageC = {
-//     notification: {
-//       title: title,
-//       body: description,
-//       image: image ? image : "",
-//     },
-//     data: {
-//       type: action,
-//     },
-//     token: fcmToken,
-//   };
-//   try {
-//     console.log("try side");
-//     if (!fcmToken) {
-//       return "User not login yet once!";
-//     }
-//     const result = await Notification.create({
-//       userId,
-//       title,
-//       message: description,
-//       targetType,
-//       action,
-//     });
-//     if (result) {
-//       admin
-//         .messaging()
-//         .send(messageC)
-//         .then(async (response) => {
-//           console.log("response admin.messaging: ", response);
-//         })
-//         .catch((err) => {
-//           console.log("err: error on sending message", err);
-//           return err;
-//         });
-//     } else {
-//       return "Failed to creaet notificatoin in table!";
-//     }
-//   } catch (error) {
-//     console.log("error on sendMessage: ", error);
-//     return error.message;
-//   }
-// };
-
-// exports.sendMultipleNotification = async (
-//   title,
-//   description,
-//   action,
-//   targetType,
-//   userId
-// ) => {
-//   try {
-//     const users = await User.find({
-//       _id: { $in: userId },
-//       role: { $ne: "admin" },
-//     });
-//     const fcmTokens = users
-//       .map((user) => user.fcmToken)
-//       .filter((token) => token);
-//     if (fcmTokens.length === 0) {
-//       console.log("No valid FCM tokens found");
-//       return "No valid FCM tokens found";
-//     }
-//     const messageC = {
-//       notification: {
-//         title: title,
-//         body: description,
-//       },
-//       data: {
-//         type: action,
-//       },
-//       tokens: fcmTokens,
-//     };
-//     const notifications = users.map((user) => ({
-//       userId: user._id,
-//       title,
-//       message: description,
-//       targetType,
-//       action,
-//     }));
-//     await Notification.insertMany(notifications);
-//     const response = await admin.messaging().sendEachForMulticast(messageC);
-//     console.log("Bulk notification sent:", response);
-//     return response;
-//   } catch (error) {
-//     console.error("Error on sendMultipleNotification:", error);
-//     return error.message;
-//   }
-// };
-
 const admin = require("firebase-admin");
 const User = require("../model/User");
+const Notification = require("../model/Notification");
 const serviceAccount = require("../firebaseSecretKeys.json");
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
 });
+
+exports.sendMultipleNotification = async (
+  title,
+  description,
+  action,
+  targetType,
+  userId,
+) => {
+  try {
+    const users = await User.find({
+      _id: { $in: userId },
+      role: { $ne: "admin" },
+    });
+    const fcmTokens = users
+      .map((user) => user.fcmToken)
+      .filter((token) => token);
+    if (fcmTokens.length === 0) {
+      console.log("No valid FCM tokens found");
+      return "No valid FCM tokens found";
+    }
+    const messageC = {
+      notification: {
+        title: title,
+        body: description,
+      },
+      data: {
+        type: action,
+      },
+      tokens: fcmTokens,
+    };
+    const response = await admin.messaging().sendEachForMulticast(messageC);
+    const notifications = users.map((user, index) => ({
+      userId: user._id,
+      title,
+      message: description,
+      targetType,
+      action,
+      status: response.responses[index]?.success ? "SENT" : "FAILED",
+    }));
+    await Notification.insertMany(notifications);
+    console.log("Bulk notification sent:", response);
+    return response;
+  } catch (error) {
+    console.error("Error on sendMultipleNotification:", error);
+    return error.message;
+  }
+};
 
 exports.sendPushNotification = async (userId, { title, body, senderId }) => {
   try {
