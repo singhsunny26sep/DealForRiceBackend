8b8633be59ccde12a4aefb4c48f24e29b8ebdf96
Devashish Rathod <rathod.devashish09@gmail.com>
2026-02-27 00:08:57 +0530

    DB cleaned

--- router/banner.js
+++ router/banner.js
@@ -1,17 +1,23 @@
-const express = require('express')
-const { getAllBanner, addBanner, updateBanner, deleteBanner, paginationBanner } = require('../controller/banner')
-const { verifyToken } = require('../middleware/authValidation')
-const bannerRouter = express.Router()
+const express = require("express");
+const {
+  getAllBanner,
+  addBanner,
+  updateBanner,
+  deleteBanner,
+  paginationBanner,
+} = require("../controller/banner");
+const { verifyToken } = require("../middleware/authValidation");
+const bannerRouter = express.Router();
 
-bannerRouter.get('/getAll', getAllBanner)
-bannerRouter.get('/getOne/:id', getAllBanner)
+bannerRouter.get("/getAll", getAllBanner);
+bannerRouter.get("/getOne/:id", getAllBanner);
 
-bannerRouter.get('/pagination', paginationBanner)
+bannerRouter.get("/pagination", paginationBanner);
 
-bannerRouter.post('/add', verifyToken, addBanner)
+bannerRouter.post("/add", verifyToken, addBanner);
 
-bannerRouter.put('/update/:id', verifyToken, updateBanner)
+bannerRouter.put("/update/:id", verifyToken, updateBanner);
 
-bannerRouter.delete('/delete/:id', verifyToken, deleteBanner)
+bannerRouter.delete("/delete/:id", verifyToken, deleteBanner);
 
-module.exports = bannerRouter
+module.exports = bannerRouter;
